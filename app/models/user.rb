58b12d696d096ae05f9d4692572b7967d47be446
Devi Firdaus Fauzi <dfauzi@apple.com>
2024-10-20 02:04:13 +0700

    add user and implement password

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_one :wallet, as: :walletable
+
+  # Include bcrypt functionality
+  has_secure_password
+
+  validates :name, :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+end
