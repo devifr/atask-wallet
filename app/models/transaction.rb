f989bc1529fbb83d6c4b8e9a84546ed6665497b4
Devi Firdaus Fauzi <dfauzi@apple.com>
2024-10-24 23:44:43 +0700

    update transaction relation and alias and add update balance of wallet

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
-  belongs_to :source_wallet, class_name: "Wallet"
-  belongs_to :target_wallet, class_name: "Wallet"
+  belongs_to :source_wallet, class_name: "Wallet", optional: true
+  belongs_to :target_wallet, class_name: "Wallet", optional: true
   belongs_to :executed_by, class_name: "User", foreign_key: "user_id"
   enum transaction_type: { credit: 0, debit: 1 }
 
@@ -10,25 +10,25 @@ class Transaction < ApplicationRecord
 
   private
     def valid_transaction
-      if transaction_type == :debit && target_wallet.nil?
+      if debit? && target_wallet.nil?
         errors.add(:base, "You must provide a target wallet for debit transactions.")
-      elsif transaction_type == :credit && source_wallet.nil?
+      elsif credit? && source_wallet.nil?
         errors.add(:base, "You must provide a source wallet for debit credit")
       end
     end
 
     def calculate_wallet
-      if transaction_type == :debit
-        update_balance(target_wallet, amount, transaction_type)
+      if debit?
+        update_balance(target_wallet)
       else
-        update_balance(source_wallet, amount, transaction_type)
+        update_balance(source_wallet)
       end
     end
 
-    def update_balance(wallet, amount, transaction_type)
+    def update_balance(wallet)
       current_balance = wallet.balance
-      new_balance = transaction_type == :credit ? current_balance - amount : current_balance + amount
-      new_balance = [ new_balance, 0 ].max  # Ensure the balance doesn't go below 0
+      new_balance = credit? ? current_balance - amount : current_balance + amount
+      new_balance = [ new_balance, 0 ].max
       wallet.update(balance: new_balance)
     end
 end
